c5ee72f32b7714702cd247a8a49b3063265d3c3e
Philip Suskin <philip.suskin@classix.de>
2022-05-08 17:06:35 +0200

    Any regular polygons

--- KochSnowflake/main.cpp
+++ KochSnowflake/main.cpp
@@ -3,7 +3,6 @@
 #include "atomicQueue.hpp"
 
 #include <math.h>
-#include <array>
 #include <future>
 #include <random>
 
@@ -12,63 +11,63 @@ atomicQueue<sf::Vector2i> queue;
 
 /* Fills the queue with points.
  */
-void fillQueue(const std::array<sf::Vector2i, 6>& hex, const sf::Vector2i& initialPoint) {
+void fillQueue(const std::vector<sf::Vector2i>& vertices, const sf::Vector2i& initialPoint) {
+  size_t verticesCount = vertices.size();
+
   std::random_device rd;
   std::mt19937 rng(rd());
-  std::uniform_int_distribution<int> uid(0, 5);
+  std::uniform_int_distribution<int> uid(0, verticesCount - 1);
 
   sf::Vector2i p = initialPoint;
   while (running) {
     int side = uid(rng);
-    sf::Vector2i a = hex[side], b = hex[(side + 1) % 6];
+    sf::Vector2i a = vertices[side], b = vertices[(side + 1) % verticesCount];
 
     p = (p + a + b) / 3;
     while (!queue.push(p) && running) {}
   }
 }
 
-std::array<sf::Vector2i, 6> createHex(float scale = 1.f) {
-  std::array<sf::Vector2i, 6> hex;
+std::vector<sf::Vector2i> createVertices(size_t verticesCount = 6, float scale = 1.f) {
+  std::vector<sf::Vector2i> vertices;
 
   float circumradius = 100 * scale;
+  float angleDelta = 360 / verticesCount;
+  float angle = 90 + angleDelta / 2;
 
-  float xDist = circumradius * cos(30 * M_PI / 180);
-  float yDist = circumradius * sin(30 * M_PI / 180);
-
-  hex[0] = sf::Vector2i(0, int(circumradius));
-  hex[1] = sf::Vector2i(int(xDist), int(yDist));
-  hex[2] = sf::Vector2i(int(xDist), int(-yDist));
-  hex[3] = sf::Vector2i(0, int(-circumradius));
-  hex[4] = sf::Vector2i(int(-xDist), int(-yDist));
-  hex[5] = sf::Vector2i(int(-xDist), int(yDist));
-
-  return hex;
-}
-
-sf::VertexArray createHexagon(std::array<sf::Vector2i, 6>& hex) {
-  sf::VertexArray hexagon(sf::LineStrip, 7);
-  for (size_t i = 0; i < 7; i++) {
-    hexagon[i].color = sf::Color::White;
-    hexagon[i].position = sf::Vector2f(hex[i % 6]);
+  for (size_t i = 0; i < verticesCount; i++) {
+    vertices.push_back(sf::Vector2i(circumradius * cos(angle * M_PI / 180), circumradius * sin(angle * M_PI / 180)));
+    angle += angleDelta;
   }
 
-  return hexagon;
+  return vertices;
+}
+
+sf::VertexArray createPolygon(const std::vector<sf::Vector2i>& vertices) {
+  size_t verticesCount = vertices.size();
+  sf::VertexArray polygon(sf::LineStrip, verticesCount + 1);
+  for (size_t i = 0; i < verticesCount + 1; i++) {
+    polygon[i].color = sf::Color::White;
+    polygon[i].position = sf::Vector2f(vertices[i % verticesCount]);
+  }
+
+  return polygon;
 }
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(800, 600), "Koch Snowflake");
 
-  std::array<sf::Vector2i, 6> hex = createHex();
+  std::vector<sf::Vector2i> vertices = createVertices(6);
   sf::Vector2i initialPoint(0, 0);
 
-  auto f = std::async(std::launch::async, fillQueue, hex, initialPoint);
+  auto f = std::async(std::launch::async, fillQueue, vertices, initialPoint);
 
   sf::View view(sf::Vector2f(0.f, 0.f), sf::Vector2f(200.f, 200.f));
   window.setView(view);
 
   window.clear(sf::Color::Black);
 
-  sf::VertexArray hexagon = createHexagon(hex);
+  sf::VertexArray polygon = createPolygon(vertices);
   std::vector<sf::Vertex> points;
 
   sf::Clock clock;
@@ -89,7 +88,7 @@ int main() {
     }
 
     window.clear();
-    window.draw(hexagon);
+    window.draw(polygon);
     for (const sf::Vertex& point : points) {
       window.draw(&point, 1, sf::Points);
     }
